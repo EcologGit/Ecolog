70419a7b7f09886214d58b3b93576bf4efcf3f94
merf-ai <nik.bolshakov.14@bk.ru>
2023-03-29 16:54:54 +0300

    'add_jwt_auth'

---
+++ users/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
+from rest_framework_simplejwt.settings import api_settings
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import NotFound
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.utils import datetime_from_epoch
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class CustomRefreshToken(RefreshToken):
+
+    def blacklist(self):
+
+        user_id = self.payload["user_id"]
+        jti = self.payload[api_settings.JTI_CLAIM]
+        exp = self.payload["exp"]
+        try:
+            user = User.objects.get(id=user_id)
+        except ObjectDoesNotExist:
+            raise NotFound('User with data not found')
+
+        token, _ = OutstandingToken.objects.get_or_create(
+            user=user,
+            jti=jti,
+            defaults={
+                "token": str(self),
+                "expires_at": datetime_from_epoch(exp),
+            },
+        )
+        
+        return BlacklistedToken.objects.get_or_create(token=token)
+    
+
+class CustomTokenRefreshSerializer(TokenRefreshSerializer):
+    token_class = CustomRefreshToken
+
+
+class CookieTokenRefreshSerializer(CustomTokenRefreshSerializer):
+    refresh = None
+    def validate(self, attrs):
+        attrs['refresh'] = self.context['request'].COOKIES.get('refresh_token')
+        if attrs['refresh']:
+            return super().validate(attrs)
+        else:
+            raise InvalidToken('No valid token found in cookie \'refresh_token\'')
---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from users.views import *
+
+urlpatterns = [
+    path('api/browser_token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/browser_refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/mobile_token/', TokenObtainPairView.as_view()),
+    path('api/mobile_refresh/', TokenRefreshView.as_view()),
+]
