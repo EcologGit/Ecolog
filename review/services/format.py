cc0e71bbd0d658b381dcfbe485325bd2ebecf38f
merf-ai <nik.bolshakov.14@bk.ru>
2023-04-03 13:00:23 +0300

    'format_fix'

--- review/services/format.py
+++ review/services/format.py
@@ -1,14 +1,11 @@
 from django.db.models import QuerySet, Model
 from review.config import OBJECT_TYPE_MAP, KM_DISTANCE_NEAR_POINT
 from rest_framework.exceptions import NotFound
+from eco.models import Routes, NatureObjects
 
-def object_type_handler(func, object_type: Model, object_id: int) -> QuerySet:
-    '''
-    Выбирает модель и применяет на неё функцию, которая выбирает объекты с object_id
-    '''
-
+def object_type_handler(func, object_type: Model, object_id: int, object_type_map=OBJECT_TYPE_MAP) -> QuerySet:
     try:
-        model_object = OBJECT_TYPE_MAP[object_type]
+        model_object = object_type_map[object_type]
         queryset = func(model_object, object_id)
     except KeyError:
         raise NotFound
