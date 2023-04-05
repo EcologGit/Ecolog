94f451111f797e176a1d20c5bcbac7e7bb278f74
merf-ai <nik.bolshakov.14@bk.ru>
2023-04-05 15:39:04 +0300

    'fix_format'

--- review/services/format.py
+++ review/services/format.py
@@ -3,7 +3,7 @@ from review.config import OBJECT_TYPE_MAP, KM_DISTANCE_NEAR_POINT
 from rest_framework.exceptions import NotFound
 from eco.models import Routes, NatureObjects
 
-def object_type_handler(object_type: Model, object_type_map=OBJECT_TYPE_MAP) -> QuerySet:
+def get_model_or_not_found_error(object_type: str, object_type_map=OBJECT_TYPE_MAP) -> QuerySet:
     try:
         model_object = object_type_map[object_type]
     except KeyError:
