302e5c6314e74fc246907019f876d97d01cdfe85
merf-ai <nik.bolshakov.14@bk.ru>
2023-05-25 01:31:08 +0300

    Добавил Allowed hosts

--- Ecolog_django/settings.py
+++ Ecolog_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = no_secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['81.163.30.36']
 
 # Application definition
 
