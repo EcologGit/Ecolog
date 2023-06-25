7d9a4a0c25093ed2e11bd946335ad18c32625ea9
merf-ai <nik.bolshakov.14@bk.ru>
2023-06-25 23:04:49 +0300

    Убрал csrf-токен

--- Ecolog_django/settings.py
+++ Ecolog_django/settings.py
@@ -49,12 +49,13 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+#Убрал csrf-токен, при необходимости вернуть!
+#'django.middleware.csrf.CsrfViewMiddleware'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
