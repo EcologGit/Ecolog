d40eca7bbd2c53668702dc6d8be2a53d5b5697b2
merf-ai <nik.bolshakov.14@bk.ru>
2023-06-30 23:57:24 +0300

    Настройки gunicorn

---
+++ config/gunicorn.conf.py
@@ -0,0 +1,8 @@
+import multiprocessing
+from Ecolog_django.settings import DEBUG
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+reload = DEBUG
+timeout = 120
+max_requests = 1000
