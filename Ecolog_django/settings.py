8f2fbc9be31e4caa26be0a63778e5e960828b3df
merf-ai <nik.bolshakov.14@bk.ru>
2023-07-01 23:20:49 +0300

    CORS (вроде принимает запросы с vue)

--- Ecolog_django/settings.py
+++ Ecolog_django/settings.py
@@ -154,8 +154,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = (
     #Убрать(надо разделить тестовый и прод сервера)
     'http://localhost:8080',
-    'http://81.163.30.36:80',
-    'http://81.163.30.36:443',
+    'http://81.163.30.36',
 )
 
 CORS_ALLOW_METHODS = [
