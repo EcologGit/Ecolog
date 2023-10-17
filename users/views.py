5b8bf11d1f180fe4100e767e1faae8f1d8a24919
merf-ai <nik.bolshakov.14@bk.ru>
2023-10-17 23:51:28 +0300

    id в апишках для токенов

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.utils.translation import gettext_lazy as _
+from Ecolog_django.settings import SECURE_COOKIE
 from .serializers import CookieTokenRefreshSerializer, CreateProfileUserSerializer
 from rest_framework.views import APIView
 from rest_framework.exceptions import APIException
@@ -15,12 +16,14 @@ class CookieTokenObtainPairView(TokenObtainPairView):
         else:
             response.data['id'] = None
         if response.data.get("refresh"):
+            print(SECURE_COOKIE.lower() == 'true')
             cookie_max_age = 3600 * 24 * 14  # 14 days
             response.set_cookie(
                 "refresh_token",
                 response.data["refresh"],
                 max_age=cookie_max_age,
                 httponly=True,
+                secure=SECURE_COOKIE.lower() == 'true',
             )
             del response.data["refresh"]
         return super().finalize_response(request, response, *args, **kwargs)
@@ -28,6 +31,10 @@ class CookieTokenObtainPairView(TokenObtainPairView):
 
 class CookieTokenRefreshView(TokenRefreshView):
     def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get("refresh"):
+            response.data['id'] = RefreshToken(response.data.get("refresh")).payload.get('user_id')
+        else:
+            response.data['id'] = None
         if response.data.get("refresh"):
             cookie_max_age = 3600 * 24 * 14  # 14 days
             response.set_cookie(
